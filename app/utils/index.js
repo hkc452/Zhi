827d98c4acc269b5dd756ca3f374e2e3563ecdb9
hkcmly <779543729@qq.com>
2016-07-27 15:35:59 +0800

    优化utils入口

--- app/utils/index.js
+++ app/utils/index.js
@@ -3,6 +3,8 @@
  */
 import consts from './consts'
 import nav from './nav'
+import * as RQ from './network'
+import * as Kstore from './kstore'
 export default{
-    consts,nav
+    consts,nav,RQ,Kstore
 }
