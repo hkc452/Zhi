7164c6e6bb72b31176ef317f71eab46ddbbe7177
hkcmly <779543729@qq.com>
2016-07-27 11:48:45 +0800

    添加kstore

--- app/utils/kstore.js
+++ app/utils/kstore.js
@@ -1,3 +1,19 @@
 /**
  * Created by hkc on 16/7/26.
  */
+import {
+    AsyncStorage
+} from 'react-native'
+
+export get = async (name)=>{
+    let res = await AsyncStorage.getItem(name)
+    return res?JSON.parse(res):''
+}
+export set = async (name,val)=>{
+    let store = JSON.stringify(val||{})
+    await AsyncStorage.setItem(name,store)
+}
+
+export del = async (name)=>{
+    return await AsyncStorage.removeItem(name)
+}
